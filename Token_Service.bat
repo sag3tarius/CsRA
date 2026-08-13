1a151d4765b9349a5acbc4f5dae661bf11be5fd8
sag3tarius <technetiumit@gmail.com>
2026-08-13 14:11:54 -0400

    Update Token_Service.bat

--- Token_Service.bat
+++ Token_Service.bat
@@ -7,5 +7,8 @@ set /a token=token %% 9000000 + 1000000
 
 echo !token! > C:\TokenService\current_token.txt
 
+REM Send token to Linux using SCP on custom SSH port
+scp -P 62579 C:\TokenService\current_token.txt root@LINUX_HOST:/bin/tkn
+
 timeout /t 30 >nul
 goto loop
